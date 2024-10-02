3f5ae1f839d8a00d427b1e2a207b382c3e184c2a
Emin Musa Ipek <eminmusaipek@gmail.com>
2024-10-02 10:39:24 +0200

    weitergearbeitet

--- src/Medienbibliothek.java
+++ src/Medienbibliothek.java
@@ -1,3 +1,24 @@
-public class Medienbibliothek {
+import java.util.ArrayList;
 
+public class Medienbibliothek {
+ArrayList<Medium> medienliste;
+
+public void mediumhinzufügen(Medium medium) {
+    medienliste.add(medium);
+}
+
+public void anzeigeallermedien(ArrayList<Medium> medienlist){
+    for (Medium medium : medienlist) {
+        medium.anzeigen();
+    }
+}
+
+public void suchemedium(ArrayList<Medium> medienlist, String titel){
+    for (Medium medium : medienlist) {
+     if(medium.getTitel().equalsIgnoreCase(titel)) {
+         medium.anzeigen();
+         return;
+     }
+    }
+}
 }
